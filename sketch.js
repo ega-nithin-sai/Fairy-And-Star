09f7b9b622ad643ab41812d9cfb8a1a500ce5150
ega-nithin-sai <77228713+ega-nithin-sai@users.noreply.github.com>
2021-02-06 11:48:02 +0530

    stage 1

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("star.png");
+	fairyImg = loadAnimation("fairyImage1.png","fairyImage2.png");
+	bgImg = loadImage("starNight.png");
+	fairyVoice = loadSound("JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+	if(keyWentDown("down")){
+		star.velocityY = 2;
+	}
+
+	if(keyDown("right")){
+		fairy.x += 3
+	}
+	else if(keyDown("left")){
+		fairy.x -= 3
+	}
+
+	if(star.y >= 490 && fairy.x >= 508){
+		star.velocityY = 0;
+	}
+
+
+
+  drawSprites();
+}
